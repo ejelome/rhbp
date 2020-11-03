b685611be63710e5d19683317e8c329f0a452c5b
ejelome <ejelome@gmail.com>
2020-11-03 18:14:40 +0200

    Use json-server and mock fetch to test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
-const App = () => <h1>hello, world</h1>;
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const initialState = () => ({ title: "" });
+  const [resource, setResource] = useState(initialState);
+
+  useEffect(() => {
+    const { REACT_APP_BASE_API_URL: baseUrl } = process.env;
+    const id = 1;
+    const path = "hello-world";
+    const endpoint = `${baseUrl}/${path}/${id}`;
+
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then(({ attributes }) =>
+        setResource((prevResource) => ({ prevResource, ...attributes }))
+      );
+  }, []);
+
+  const { title } = resource;
+
+  return <h1>{title}</h1>;
+};
 
 export default App;
