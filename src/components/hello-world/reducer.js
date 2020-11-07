06ff37b25c5d5c7c99f9c2154cf9d0640dc249fe
ejelome <ejelome@gmail.com>
2020-11-07 21:09:44 +0100

    Fix affected files with eslint

--- src/components/hello-world/reducer.js
+++ src/components/hello-world/reducer.js
@@ -2,7 +2,7 @@ import { SET_TITLE } from "./actionTypes";
 
 const reducer = (state, { type, payload: { title } }) =>
   ({
-    SET_TITLE: { ...state, title },
+    [SET_TITLE]: { ...state, title },
   }[type] || state);
 
 export default reducer;
